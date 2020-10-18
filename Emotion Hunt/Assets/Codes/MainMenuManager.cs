33bff25206a5631eb104aead0ca9a529bc7dd40b
AtenB <atenb3f@gmail.com>
2020-10-18 17:42:29 +0900

    Menu Frame

--- Emotion Hunt/Assets/Codes/MainMenuManager.cs
+++ Emotion Hunt/Assets/Codes/MainMenuManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-/*
+/*      [ Button ]
+ *      
  * < Button Set Hieararchy >
  * 1. Play Button
  * 2. Collect Button
@@ -14,14 +15,36 @@ using UnityEngine.EventSystems;
  * <Sub Button Set Hieararchy>
  * 1. Setting Icon
  * 2. Exit Icon
+ * 
+ * <Play Menu Button Set Hieararchy>
+ * 1. New Game Button
+ * 2. Load Game Button
+ * 3. Colse Button
+ * 
+ * <Setting Menu Button Set Hieararchy
+ * 1. Develop Info Button
+ * 2. Close Button
  */
+
+/*      [Slider]
+ * 
+ */
+
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject Btn;
-    
+    public GameObject MainBtn;      // Play, Collect, Setting, Exit Button (in "TitlePanel"-"ButtonSet")
+    public GameObject SubBtn;       // Setting Icon, Exit Icon Button (in "TitlePanel"-"SubButtonSet")
+    public GameObject PlayObj;      // Play Menu Object
+    public GameObject SettingObj;   // Setting Menu Object
+    public GameObject SystemPanel;  // System Panel
+
     void Start()
     {
-        
+        Button[] MainBtnArr = MainBtn.GetComponentsInChildren<Button>();
+        Button[] SubBtnArr = SubBtn.GetComponentsInChildren<Button>();
+
+        // Panel Disable
+        SystemPanelEnable(false);
     }
 
     void Update()
@@ -29,9 +52,51 @@ public class MainMenuManager : MonoBehaviour
         
     }
 
+    private void PlayMenu(bool En)
+    {
+        if(En)
+        {
+            PlayObj.SetActive(true);
+
+            // System Panel Blur Effect On
+            SystemPanelEnable(true);
+
+            // Enable Play Menu Panel
+
+        }
+        else
+        {
+            PlayObj.SetActive(false);
+
+            // System Panel Blur Effect On
+            SystemPanelEnable(false);
+
+            // Disable Play Menu Panel
+        }
+
+    }
+
+    private void SystemPanelEnable(bool En)
+    {
+        if(En)
+            SystemPanel.GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f, 0.4f);
+        else
+            SystemPanel.GetComponent<Image>().color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+    }
+
     private void ButtonStatus()
     {
-        Button[] BtnArr = Btn.GetComponentsInChildren<Button>();
+        
+    }
+
+    private void PlayMenu()
+    {
+
+    }
+
+    private void SettingMenu()
+    {
+
     }
 
     public void ResetSelected()
