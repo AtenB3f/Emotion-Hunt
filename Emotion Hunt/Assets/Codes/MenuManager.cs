58f2a7a4af0c9b1b25ad8297c5c96733277687d7
AtenB <atenb3f@gmail.com>
2022-01-11 00:52:34 +0900

    Feat: 설정 메뉴 스크롤 적용, 메뉴 매니저 작성 및 적용

---
+++ Emotion Hunt/Assets/Codes/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menuPanel;
+
+    [SerializeField]
+    private GameObject blurPanel;
+
+    [SerializeField]
+    private GameObject[] menu = { };
+
+    void Start()
+    {
+        menuPanel.SetActive(false);
+        blurPanel.SetActive(false);
+
+        foreach (GameObject obj in menu)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public void OnMenu(int index)
+    {
+        if (index >= menu.Length)
+            return;
+
+        menuPanel.SetActive(true);
+        blurPanel.SetActive(true);
+
+        menu[index].SetActive(true);
+    }
+
+    public void OffMenu()
+    {
+        menuPanel.SetActive(false);
+        blurPanel.SetActive(false);
+
+        foreach (GameObject obj in menu)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+}
